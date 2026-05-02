6722f1fb70cb95243fedf8785654727e65c2a5c1
SagarS2005 <sagarsahuji000@gmail.com>
2026-05-02 10:46:11 +0530

    program 9 : Singly LinkedList

---
+++ pr9.java
@@ -0,0 +1,40 @@
+ class pr9 {
+    class node{
+        int value;
+        node next;
+
+        node(int value){
+            this.value=value;
+            this.next=null;
+        }
+    }
+
+    static node head =null;
+    static node tail =null;
+    public void addNode(int value){
+        node newNode=new node(value);
+        if (head == null){
+            head=tail= newNode;
+        } 
+        tail.next=newNode;
+        tail=newNode;
+
+    }
+    public void display(){
+        node temp=head;
+        while (temp!=null) {
+            System.out.print(temp.value+ "=>");
+            temp=temp.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        pr9 ob=new pr9();
+        ob.addNode(1);
+        ob.addNode(2);
+        ob.addNode(3);
+        ob.addNode(4);
+        ob.display();
+    }
+}
